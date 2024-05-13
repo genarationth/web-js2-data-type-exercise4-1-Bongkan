eb8ec84985fde56bea5f29045d0b3c46b3cf0b9f
Bongkan <bongkan.jj@gmail.com>
2024-05-13 14:33:06 +0700

    finished 2

--- js.js
+++ js.js
@@ -13,4 +13,12 @@ const product = {
 };
 
 console.log(product.product_id);
+console.log(product.product_name);
+console.log(product.quantity_in_stock);
+console.log(product.unit_price);
+console.log(product.product_brand);
+console.log(product.product_description);
+console.log(product.product_description.size_or_quantity);
+console.log(product.product_description.taste_or_scent);
 console.log(product.product_description.colour);
+console.log(product.product_description.additional_description);
